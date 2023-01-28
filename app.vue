5365b4634653fc8cf6e5e5fb5029fea42924ffe2
CelsonF <celsondev@outlook.com>
2023-01-28 15:35:35 -0300

    add cdn config

--- app.vue
+++ app.vue
@@ -17,6 +17,12 @@ useHead({
 	},
 	link: [
 		{
+			href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css",
+			rel: "stylesheet",
+			integrity: "sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD",
+			crossorigin: "anonymous",
+		},
+		{
 			rel: "stylesheet",
 			href: "https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;500;700;800&display=swap"
 		},
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,5 @@ export default defineNuxtConfig({
 css:[
 	'@/assets/css/base.css',
 	'@/assets/fonts/typography.css',
-	'@/node_modules/bootstrap/dist/css/bootstrap.min.css',
 ],
 })
