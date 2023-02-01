5208dc2f737f17dc3b08c051d74b2f9f92b5935b
CelsonF <celson338540@gmail.com>
2023-02-01 14:47:05 -0300

    add seo index configuration

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import Servicos from "~/pages/servicos.vue"
+</script>
+
 <template>
 	<main>
 		<div class="container-fluid p-0" id="home">
@@ -8,6 +12,7 @@
 					alt="Lamborguine azul no fundo" />
 			</div>
 		</div>
+
 		<section class="container-xl py-5">
 			<h2 class="text-center py-5 fw-bold">
 				Como são as consultas da Autocredit ?
@@ -66,184 +71,7 @@
 			</div>
 		</section>
 
-		<section
-			class="container-fluid low-opacity-bg-image pb-5"
-			id="servico">
-			<h2 class="text-white text-center fw-bold pt-4">Nossos Produtos</h2>
-
-			<div class="container">
-				<div class="row">
-					<div class="col-12 col-md-4 col-lg-3 mt-5">
-						<div class="card align-items-center">
-							<div
-								class="icons-service d-flex justify-content-center align-items-center my-4">
-								<img
-									class="img-fluid icons-witdh-service"
-									src="../assets/images/icons/relatorio-credito.svg"
-									alt="Relatório de Crédito" />
-							</div>
-							<div class="card-body">
-								<h5 class="card-title">Relatório de Crédito</h5>
-							</div>
-						</div>
-					</div>
-					<div class="col-12 col-md-4 col-lg-3 mt-5">
-						<div class="card align-items-center">
-							<div
-								class="icons-service d-flex justify-content-center align-items-center my-4">
-								<img
-									class="img-fluid icons-witdh-service"
-									src="../assets/images/icons/license-drive.svg"
-									alt="Consulta Veicular" />
-							</div>
-							<div class="card-body">
-								<h5 class="card-title">Consulta Veicular</h5>
-							</div>
-						</div>
-					</div>
-					<div class="col-12 col-md-4 col-lg-3 mt-5">
-						<div class="card align-items-center">
-							<div
-								class="icons-service d-flex justify-content-center align-items-center my-4">
-								<img
-									class="img-fluid icons-witdh-service"
-									src="../assets/images/icons/infobusca.svg"
-									alt="Infobusca" />
-							</div>
-							<div class="card-body">
-								<h5 class="card-title">Infobusca</h5>
-							</div>
-						</div>
-					</div>
-					<div class="col-12 col-md-4 col-lg-3 mt-5">
-						<div class="card align-items-center">
-							<div
-								class="icons-service d-flex justify-content-center align-items-center my-4">
-								<img
-									class="img-fluid icons-witdh-service"
-									src="../assets/images/icons/negativacao.svg"
-									alt="Negativação" />
-							</div>
-							<div class="card-body">
-								<h5 class="card-title">Negativação</h5>
-							</div>
-						</div>
-					</div>
-					<div class="col-12 col-md-4 col-lg-3 mt-5">
-						<div class="card align-items-center">
-							<div
-								class="icons-service d-flex justify-content-center align-items-center my-4">
-								<img
-									class="img-fluid icons-witdh-service"
-									src="../assets/images/icons/protesto-titulos.svg"
-									alt="Protesto de Títulos" />
-							</div>
-							<div class="card-body">
-								<h5 class="card-title">Protesto de Títulos</h5>
-							</div>
-						</div>
-					</div>
-					<div class="col-12 col-md-4 col-lg-3 mt-5">
-						<div class="card align-items-center">
-							<div
-								class="icons-service d-flex justify-content-center align-items-center my-4">
-								<img
-									class="img-fluid icons-witdh-service"
-									src="../assets/images/icons/acoes-judiciais.svg"
-									alt="Ações Judiciais" />
-							</div>
-							<div class="card-body">
-								<h5 class="card-title">Ações Judiciais</h5>
-							</div>
-						</div>
-					</div>
-					<div class="col-12 col-md-4 col-lg-3 mt-5">
-						<div class="card align-items-center">
-							<div
-								class="icons-service d-flex justify-content-center align-items-center my-4">
-								<img
-									class="img-fluid icons-witdh-service"
-									src="../assets/images/icons/pesquisa-imoveis.svg"
-									alt="Localizador de Imóveis" />
-							</div>
-							<div class="card-body">
-								<h5 class="card-title">Localizador de Imóveis</h5>
-							</div>
-						</div>
-					</div>
-					<div class="col-12 col-md-4 col-lg-3 mt-5">
-						<div class="card align-items-center">
-							<div
-								class="icons-service d-flex justify-content-center align-items-center my-4">
-								<img
-									class="img-fluid icons-witdh-service"
-									src="../assets/images/icons/certificado-digital.svg"
-									alt="Certificado Digital" />
-							</div>
-							<div class="card-body">
-								<h5 class="card-title">Certificado Digital</h5>
-							</div>
-						</div>
-					</div>
-					<div class="col-12 col-md-4 col-lg-3 mt-5">
-						<div class="card align-items-center">
-							<div
-								class="icons-service d-flex justify-content-center align-items-center my-4">
-								<img
-									class="img-fluid icons-witdh-service"
-									src="../assets/images/icons/atpv-e.svg"
-									alt="ATPV-E" />
-							</div>
-							<div class="card-body">
-								<h5 class="card-title">ATPV-E</h5>
-							</div>
-						</div>
-					</div>
-					<div class="col-12 col-md-4 col-lg-3 mt-5">
-						<div class="card align-items-center">
-							<div
-								class="icons-service d-flex justify-content-center align-items-center my-4">
-								<img
-									class="img-fluid icons-witdh-service"
-									src="../assets/images/icons/documento-carro.svg"
-									alt="CRLV Emissão Documento Carro" />
-							</div>
-							<div class="card-body">
-								<h5 class="card-title">CRLV Emissão Documento Carro</h5>
-							</div>
-						</div>
-					</div>
-					<div class="col-12 col-md-4 col-lg-3 mt-5">
-						<div class="card align-items-center">
-							<div
-								class="icons-service d-flex justify-content-center align-items-center my-4">
-								<img
-									class="img-fluid icons-witdh-service"
-									src="../assets/images/icons/venda-carro.svg"
-									alt="Comunicado de Vendas de Veículos" />
-							</div>
-							<div class="card-body">
-								<h5 class="card-title">Comunicado de Vendas de Veículos</h5>
-							</div>
-						</div>
-					</div>
-					<div class="col-12 col-md-4 col-lg-3 mt-5">
-						<div class="card align-items-center">
-							<div
-								class="icons-service d-flex justify-content-center align-items-center my-4">
-								<img
-									class="img-fluid icons-witdh-service"
-									src="../assets/images/icons/pesquisa-carro.svg"
-									alt="Localizador de Veículos" />
-							</div>
-							<div class="card-body">
-								<h5 class="card-title">Localizador de Veículos</h5>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
+		<Servicos/>
 
 		<section class="container-fluid d-flex low-bg-image-about d-flex flex-column justify-content-center pt-5 "
 			id="sobre">
@@ -292,14 +120,7 @@
 	background-color: var(--color-primary-1);
 }
 
-.icons-service {
-	width: 6.25rem;
-	height: 6.25rem;
 
-	border-radius: 50%;
-
-	background-color: var(--color-primary-3);
-}
 
 .icon-whatsapp {
 	width: 2.5rem;
@@ -322,34 +143,6 @@
 	box-shadow: inset 0px 1px 5px #141132;
 }
 
-.icons-witdh-service {
-	width: 4rem;
-}
-
-.low-opacity-bg-image {
-	--bg-image: url("../assets/images/business-man-fullhd.jpg");
-	--bg-image-opacity: 0.25;
-
-	background-color: var(--color-primary-3);
-
-	position: relative;
-
-	isolation: isolate;
-}
-
-.low-opacity-bg-image::after {
-	content: "";
-	position: absolute;
-	z-index: -1;
-	inset: 0;
-
-	opacity: var(--bg-image-opacity);
-
-
-	background-image: var(--bg-image);
-	background-size: cover;
-	background-position: center;
-}
 
 
 .low-bg-image-about {
---
+++ pages/servicos.vue
@@ -0,0 +1,231 @@
+<script setup lang="ts">
+useHead({
+    meta:[
+        {name:'description',content:'Os serviços da Autocredit contém, vários tipos de consultas como: Relatório de Crédito, Consulta veicular, infobusca, Negativação, Protesto de Títulos , Ações Judiciais, Localizador de imóveis, certificado digital , ATPV-E , CRLV - Emissão de documento de carro , Comunicado de vendas de veículos, localizador de veículos'}
+    ]
+})
+</script>
+
+<template>
+    <section
+			class="container-fluid low-opacity-bg-image pb-5"
+			id="servico">
+			<h2 class="text-white text-center fw-bold pt-4">Nossos Produtos</h2>
+
+			<div class="container">
+				<div class="row">
+					<div class="col-12 col-md-4 col-lg-3 mt-5">
+						<div class="card align-items-center">
+							<div
+								class="icons-service d-flex justify-content-center align-items-center my-4">
+								<img
+									class="img-fluid icons-witdh-service"
+									src="../assets/images/icons/relatorio-credito.svg"
+									alt="Relatório de Crédito" />
+							</div>
+							<div class="card-body">
+								<h5 class="card-title">Relatório de Crédito</h5>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 col-md-4 col-lg-3 mt-5">
+						<div class="card align-items-center">
+							<div
+								class="icons-service d-flex justify-content-center align-items-center my-4">
+								<img
+									class="img-fluid icons-witdh-service"
+									src="../assets/images/icons/license-drive.svg"
+									alt="Consulta Veicular" />
+							</div>
+							<div class="card-body">
+								<h5 class="card-title">Consulta Veicular</h5>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 col-md-4 col-lg-3 mt-5">
+						<div class="card align-items-center">
+							<div
+								class="icons-service d-flex justify-content-center align-items-center my-4">
+								<img
+									class="img-fluid icons-witdh-service"
+									src="../assets/images/icons/infobusca.svg"
+									alt="Infobusca" />
+							</div>
+							<div class="card-body">
+								<h5 class="card-title">Infobusca</h5>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 col-md-4 col-lg-3 mt-5">
+						<div class="card align-items-center">
+							<div
+								class="icons-service d-flex justify-content-center align-items-center my-4">
+								<img
+									class="img-fluid icons-witdh-service"
+									src="../assets/images/icons/negativacao.svg"
+									alt="Negativação" />
+							</div>
+							<div class="card-body">
+								<h5 class="card-title">Negativação</h5>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 col-md-4 col-lg-3 mt-5">
+						<div class="card align-items-center">
+							<div
+								class="icons-service d-flex justify-content-center align-items-center my-4">
+								<img
+									class="img-fluid icons-witdh-service"
+									src="../assets/images/icons/protesto-titulos.svg"
+									alt="Protesto de Títulos" />
+							</div>
+							<div class="card-body">
+								<h5 class="card-title">Protesto de Títulos</h5>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 col-md-4 col-lg-3 mt-5">
+						<div class="card align-items-center">
+							<div
+								class="icons-service d-flex justify-content-center align-items-center my-4">
+								<img
+									class="img-fluid icons-witdh-service"
+									src="../assets/images/icons/acoes-judiciais.svg"
+									alt="Ações Judiciais" />
+							</div>
+							<div class="card-body">
+								<h5 class="card-title">Ações Judiciais</h5>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 col-md-4 col-lg-3 mt-5">
+						<div class="card align-items-center">
+							<div
+								class="icons-service d-flex justify-content-center align-items-center my-4">
+								<img
+									class="img-fluid icons-witdh-service"
+									src="../assets/images/icons/pesquisa-imoveis.svg"
+									alt="Localizador de Imóveis" />
+							</div>
+							<div class="card-body">
+								<h5 class="card-title">Localizador de Imóveis</h5>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 col-md-4 col-lg-3 mt-5">
+						<div class="card align-items-center">
+							<div
+								class="icons-service d-flex justify-content-center align-items-center my-4">
+								<img
+									class="img-fluid icons-witdh-service"
+									src="../assets/images/icons/certificado-digital.svg"
+									alt="Certificado Digital" />
+							</div>
+							<div class="card-body">
+								<h5 class="card-title">Certificado Digital</h5>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 col-md-4 col-lg-3 mt-5">
+						<div class="card align-items-center">
+							<div
+								class="icons-service d-flex justify-content-center align-items-center my-4">
+								<img
+									class="img-fluid icons-witdh-service"
+									src="../assets/images/icons/atpv-e.svg"
+									alt="ATPV-E" />
+							</div>
+							<div class="card-body">
+								<h5 class="card-title">ATPV-E</h5>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 col-md-4 col-lg-3 mt-5">
+						<div class="card align-items-center">
+							<div
+								class="icons-service d-flex justify-content-center align-items-center my-4">
+								<img
+									class="img-fluid icons-witdh-service"
+									src="../assets/images/icons/documento-carro.svg"
+									alt="CRLV Emissão Documento Carro" />
+							</div>
+							<div class="card-body">
+								<h5 class="card-title">CRLV Emissão Documento Carro</h5>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 col-md-4 col-lg-3 mt-5">
+						<div class="card align-items-center">
+							<div
+								class="icons-service d-flex justify-content-center align-items-center my-4">
+								<img
+									class="img-fluid icons-witdh-service"
+									src="../assets/images/icons/venda-carro.svg"
+									alt="Comunicado de Vendas de Veículos" />
+							</div>
+							<div class="card-body">
+								<h5 class="card-title">Comunicado de Vendas de Veículos</h5>
+							</div>
+						</div>
+					</div>
+					<div class="col-12 col-md-4 col-lg-3 mt-5">
+						<div class="card align-items-center">
+							<div
+								class="icons-service d-flex justify-content-center align-items-center my-4">
+								<img
+									class="img-fluid icons-witdh-service"
+									src="../assets/images/icons/pesquisa-carro.svg"
+									alt="Localizador de Veículos" />
+							</div>
+							<div class="card-body">
+								<h5 class="card-title">Localizador de Veículos</h5>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+</template>
+
+<style scoped>
+
+
+.icons-service {
+	width: 6.25rem;
+	height: 6.25rem;
+
+	border-radius: 50%;
+
+	background-color: var(--color-primary-3);
+}
+
+.icons-witdh-service {
+	width: 4rem;
+}
+
+.low-opacity-bg-image {
+	--bg-image: url("../assets/images/business-man-fullhd.jpg");
+	--bg-image-opacity: 0.25;
+
+	background-color: var(--color-primary-3);
+
+	position: relative;
+
+	isolation: isolate;
+}
+
+.low-opacity-bg-image::after {
+	content: "";
+	position: absolute;
+	z-index: -1;
+	inset: 0;
+
+	opacity: var(--bg-image-opacity);
+
+
+	background-image: var(--bg-image);
+	background-size: cover;
+	background-position: center;
+}
+
+</style>
