d01068499ef84aff06248521570268ff14a1d817
CelsonF <celsondev@outlook.com>
2023-06-22 00:17:07 -0300

    add images and text adjust

--- components/layout/carousel.vue
+++ components/layout/carousel.vue
@@ -6,6 +6,7 @@
             <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
             <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
             <button type="button" data-bs-target="#carousel" data-bs-slide-to="3" aria-label="Slide 4"></button>
+            <button type="button" data-bs-target="#carousel" data-bs-slide-to="4" aria-label="Slide 5"></button>
         </div>
         <div class="carousel-inner">
             <div class="carousel-item active">
@@ -13,15 +14,15 @@
                 <div class="carousel-caption text-start">
                     <div class="mb-0 fs-title"> Relatório de Crédito </div>
                     <ul class="mb-1 list-caption ps-0">
-                            <li class="list-items">
-                                Restrição Comercial
-                            </li>
-                            <li class="list-items">
-                                Pendencia financeira
-                            </li>
-                            <li class="list-items">
-                                Protesto Nacional
-                            </li>
+                        <li class="list-items">
+                            Restrição Comercial
+                        </li>
+                        <li class="list-items">
+                            Pendencia financeira
+                        </li>
+                        <li class="list-items">
+                            Protesto Nacional
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -31,46 +32,66 @@
                 <div class="carousel-caption text-end">
                     <div class="mb-0 fs-title"> Ações Judiciais </div>
                     <ul class="mb-1 list-caption s-0">
-                            <li class="list-items">
-                                Antecedentes criminais
-                            </li>
-                            <li class="list-items">
-                                Ações Trabalhista
-                            </li>
+                        <li class="list-items">
+                            Antecedentes criminais
+                        </li>
+                        <li class="list-items">
+                            Ações Trabalhista
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="carousel-item">
+                <img src="../../assets/images/slide-10.png" class="d-block w-100" alt="Pessoa com a mão no volante">
+                <div class="carousel-caption text-start">
+                    <div class="mb-0 fs-title">
+                        Negativação e Protesto Online
+                    </div>
+                    <ul class="mb-1 list-caption ps-0">
+                        <li class="list-items">
+                            Negativação a nível nacional
+                        </li>
+                        <li class="list-items">
+                            Recupera até 80% dos inadimplentes
+                        </li>
+                        <li class="list-items">
+                            Protesto rápido e fácil
+                        </li>
                     </ul>
                 </div>
             </div>
             <div class="carousel-item">
                 <img src="../../assets/images/slide-6.png" class="d-block w-100" alt="Pessoa com a mão no volante">
-                <div class="carousel-caption text-start">
+                <div class="carousel-caption text-end">
                     <div class="mb-0 fs-title"> Consulta veicular </div>
                     <ul class="mb-1 list-caption ps-0">
-                            <li class="list-items">
-                                Leilão/sinistro
-                            </li>
-                            <li class="list-items">
-                                Débitos e multas
-                            </li>
-                            <li class="list-items">
-                                Histórico de roubo e furto
-                            </li>
-                            <li class="list-items">
-                                Renainf
-                            </li>
+                        <li class="list-items">
+                            Leilão/sinistro
+                        </li>
+                        <li class="list-items">
+                            Débitos e multas
+                        </li>
+                        <li class="list-items">
+                            Histórico de roubo e furto
+                        </li>
+                        <li class="list-items">
+                            Renainf
+                        </li>
                     </ul>
                 </div>
             </div>
             <div class="carousel-item">
                 <img src="../../assets/images/slide-5.png" class="d-block w-100" alt="Pessoa entrando em um carro">
-                <div class="carousel-caption text-end">
+                <div class="carousel-caption text-start">
                     <div class="mb-0 fs-title"> Comunicado de Vendas </div>
                     <ul class="mb-1 list-caption s-0">
-                            <li class="list-items">
-                                ATPV-E
-                            </li>
-                            <li class="list-items">
-                                CRLV
-                            </li>
+                        <li class="list-items">
+                            ATPV-E
+                        </li>
+                        <li class="list-items">
+                            CRLV
+                        </li>
                     </ul>
                 </div>
             </div>
@@ -102,12 +123,12 @@
     margin-top: 8rem;
 }
 
-.fs-title{
-        font-size: 1.2rem;
+.fs-title {
+    font-size: 1.2rem;
 }
-    
+
 .list-items {
-        font-size: 0.7rem;
+    font-size: 0.7rem;
 }
 
 .list-caption li {
@@ -118,10 +139,10 @@
 
 /* Small devices (portrait tablets and large phones, 600px and up) */
 @media only screen and (min-width: 600px) {
-    .fs-title{
+    .fs-title {
         font-size: 2rem;
     }
-    
+
     .list-items {
         font-size: 1rem;
     }
@@ -132,16 +153,12 @@
         margin-top: 0;
     }
 
-    .fs-title{
+    .fs-title {
         font-size: 4rem;
     }
-    
+
     .list-items {
         font-size: 2rem;
     }
 }
-
-
-
-
 </style>
